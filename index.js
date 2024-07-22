3e9e21b30563047a739d2a269564bc6126590bf5
Nadia Ibrahim <nadiaibrahim400@gmail.com>
2024-07-22 23:23:16 +0000

    if statement

---
+++ index.js
@@ -0,0 +1,33 @@
+//if statement = a basic form of decision making
+//               if a decision is true, then do something (execute some line of code)
+//               if not, then don't do it!
+
+/*
+let age = 70;
+
+if(age >=65){
+    console.log("You are a senior citizen!")
+}
+
+else if(age >=18){
+    console.log("You are an adult!");
+}
+
+else if(age < 0){
+    console.log("YOU HAVEN'T BEEN BORN YET!");
+}
+
+
+else{
+    console.log("You are a child!")
+}
+*/
+
+let online = true;
+
+if(online){
+    console.log("You are online!");
+}
+else{
+    console.log("You are offline!");
+}
